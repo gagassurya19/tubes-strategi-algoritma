013e03c7c25a7aa7c0dc84e4c359eaf41d6e4050
Gagas Surya Laksana <gagassurya19@gmail.com>
2024-05-15 20:57:16 +0700

    fix/optional image

--- src/app/(page)/calculation/inputBarang.js
+++ src/app/(page)/calculation/inputBarang.js
@@ -84,7 +84,7 @@ export default function InputBarang() {
                 <div className="flex items-center">
                   <img
                     className="filter saturate-0 group-hover/item:saturate-100 inline-block flex-shrink-0 size-[62px] rounded-lg"
-                    src={item.foto}
+                    src={item.foto || "https://via.placeholder.com/150"}
                     alt="Image Description"
                   />
                   <div className="ms-3">
@@ -191,7 +191,6 @@ export default function InputBarang() {
                   id="foto"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="http://example.com/image.png"
-                  required
                 />
               </div>
               <div>
