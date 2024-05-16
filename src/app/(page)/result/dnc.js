b503b19569fa262e53395c497ba88fbb3051c233
Gagas Surya Laksana <gagassurya19@gmail.com>
2024-05-16 21:15:47 +0700

    add/dummy result

--- src/app/(page)/result/dnc.js
+++ src/app/(page)/result/dnc.js
@@ -4,6 +4,22 @@ export default function Dnc() {
       <p className="text-xl font-semibold border-b border-gray-400 dark:border-gray-700">
         Divide and Conquer
       </p>
+      <p>
+        Hasil Algoritma Divide & Conquer:
+        <br />
+        Sarung Tangan: 5 buah
+        <br />
+        Topi Baseball: 3 buah
+        <br />
+        Kacamata Hitam: 0 buah
+        <br />
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        <ul>
+          <li>Total jumlah: 8 barang</li>
+          <li>Total Harga: RP950.000</li>
+          <li>Sisa Modal: Rp50.000</li>
+        </ul>
+      </p>
     </div>
   );
 }
--- src/app/(page)/result/greedy.js
+++ src/app/(page)/result/greedy.js
@@ -4,6 +4,19 @@ export default function Greedy() {
       <p className="text-xl font-semibold border-b border-gray-400 dark:border-gray-700">
         Greedy
       </p>
+      <p>
+        Hasil Algoritma Greedy:
+        <br />
+        Sarung Tangan: 5 buah
+        <br />
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        <br />
+        Total Jumlah: 5 buah
+        <br />
+        Total Harga: Rp500000
+        <br />
+        Sisa Modal: Rp500000
+      </p>
     </div>
   );
 }
