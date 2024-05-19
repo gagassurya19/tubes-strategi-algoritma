41ae1874bdd7f0a57afa011ace62f3abafe55867
Gagas Surya Laksana <gagassurya19@gmail.com>
2024-05-20 05:51:33 +0700

    fix/typo

--- src/app/(page)/result/page.js
+++ src/app/(page)/result/page.js
@@ -35,7 +35,7 @@ export default function Result() {
   return (
     <div className="w-full justify-between mt-10">
       <p className="text-xl font-semibold border-b border-gray-400 dark:border-gray-700 mb-5">
-        ANALASIS ALGORITMA
+        ANALISIS ALGORITMA
       </p>
       <div className="flex flex-col lg:flex-row w-full justify-between">
         <Greedy data={result.greedy} jumlah_barang={result.jumlah_barang} />
