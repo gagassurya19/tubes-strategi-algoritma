0c355aca93f3e31cc392008b2f600724cb50f63f
Gagas Surya Laksana <gagassurya19@gmail.com>
2024-05-21 12:27:51 +0700

    add/google-analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
 import "./globals.css";
+import {GoogleAnalytics} from "./utility/GoogleAnalytics";
 
 export default function Layout({ children }) {
   return (
     <html suppressHydrationWarning>
+      <GoogleAnalytics />
       <body>
         <main className="flex min-h-screen flex-col items-center justify-between p-3 sm:p-24 bg-white text-black dark:bg-gray-800 dark:text-white">
           <div className="max-w-5xl w-full">
