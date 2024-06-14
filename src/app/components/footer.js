c2b5ece7c4c588a50e05a5bbf2ce9a1c9a4b0157
Gagassurya19 <gagassurya19@gmail.com>
2024-06-14 08:36:31 +0700

    Update footer.js kelompok 9 -> kelompok 10

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,7 +4,7 @@ return (
         <div className="container mx-auto py-4"></div>
         <div className="flex items-center justify-center font-mono text-sm">
                 <p className="text-sm mt-1 text-center">
-                    Copyright 2024<br/>Kelompok 9 | IF-46-10<br/>Sarjana Informatika - Telkom University
+                    Copyright 2024<br/>Kelompok 10 | IF-46-10<br/>Sarjana Informatika - Telkom University
                 </p>
             </div>
         </div>
